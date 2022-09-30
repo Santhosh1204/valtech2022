a572356be112b7fb5e1c71b01f781df24fda7ff6
Santhosh Kumar A <Santhosh.A@valtech.com>
2022-09-30 16:15:10 +0530

    first maven project

--- devops/src/devops/Airthmatic.java
+++ devops/src/devops/Airthmatic.java
@@ -1,5 +1,5 @@
 package devops;
 
-public interface Airthmatic {
+public class Airthmatic {
 
 }
---
+++ maven-example/src/main/java/com/valtech/learning/maven/example/App.java
@@ -0,0 +1,13 @@
+package com.valtech.learning.maven.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
