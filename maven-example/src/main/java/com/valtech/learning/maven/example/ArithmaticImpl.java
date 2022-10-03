d23d5479c5df47ac1cd2d5ffb75ecdffb76c916a
Santhosh Kumar A <Santhosh.A@valtech.com>
2022-10-03 12:08:48 +0530

    main

---
+++ maven-example/src/main/java/com/valtech/learning/maven/example/ArithmaticImpl.java
@@ -0,0 +1,20 @@
+package com.valtech.learning.maven.example;
+
+public class ArithmaticImpl implements Arithmetic{
+public int add(int a, int b) {
+	// TODO Auto-generated method stub
+	return a+b;
+}
+public int sub(int a, int b) {
+		// TODO Auto-generated method stub
+		return a-b;
+	}
+public int mul(int a, int b) {
+		// TODO Auto-generated method stub
+		return a*b;
+	}
+public int div(int a, int b) {
+		// TODO Auto-generated method stub
+		return a/b;
+	}
+}
---
+++ valtech-mainproject/src/main/java/com/valtech/training/valtech/mainproject/App.java
@@ -0,0 +1,13 @@
+package com.valtech.training.valtech.mainproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
